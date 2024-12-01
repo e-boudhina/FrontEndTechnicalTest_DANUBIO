62a798fb1f693ad7ac23dbac9cea027c8f50ba90
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2024-12-01 12:15:56 +0100

    feat: splitting api calls into 2 seprate urls - OnMount and OnQuerySearch

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,36 @@
-export const fetchCharacters = async () => {
+// Fetch default characters 
+export const fetchDefaultCharacters = async () => {
   try {
-    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const response = await fetch('https://rickandmortyapi.com/api/character/?page=1');
     const data = await response.json();
-    // Return only the first 10 characters
-    return data.results.slice(0, 10);
+    return data.results; 
   } catch (error) {
-    console.error('Error fetching characters from the API:', error);
-    return []; // Return an empty array in case of an error
+    console.error('Error fetching default characters:', error);
+    return [];
+  }
+};
+
+// Fetch filtered characters based on query parameters
+export const fetchFilteredCharacters = async (filters) => {
+  const { name = '', status = '', species = '', type = '', gender = '' } = filters;
+
+  // Constructing the query string based on provided filters
+  //Still in progess
+  const query = new URLSearchParams({
+    name,
+    status,
+    species,
+    type,
+    gender,
+  }).toString();
+
+  try {
+    console.log("fetching by query");
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?${query}`);
+    const data = await response.json();
+    return data.results || []; // Return filtered results or empty array
+  } catch (error) {
+    console.error('Error fetching filtered characters:', error);
+    return [];
   }
 };
